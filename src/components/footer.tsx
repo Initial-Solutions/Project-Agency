0ccb5431de751168a7ee7818963e87da9a21e170
Louis Bellefemine <bellefemine.louis@gmail.com>
2025-01-30 00:30:08 +0100

    converted a to links & fix mobile

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,10 @@
+import { Link } from '@tanstack/react-router';
+
 export function Footer() {
   return (
     <footer
       className={
-        'flex w-full flex-col items-center justify-center gap-8 px-8 py-8 md:py-16'
+        'relative flex w-full flex-col items-center justify-center gap-8 px-8 py-8 pb-4 md:py-16 md:pb-8'
       }
     >
       <div
@@ -10,40 +12,59 @@ export function Footer() {
           'flex w-3/4 flex-col justify-between gap-8 sm:w-full sm:flex-row md:w-3/4'
         }
       >
-        <div className={'flex flex-col gap-2'}>
+        <div className={'flex flex-col gap-4'}>
           <h4 className={'text-lg font-semibold italic'}>Our services</h4>
           <div className={'flex flex-col gap-2 text-sm font-light'}>
-            <a>E-commerce</a>
-            <a>Corporate Website</a>
-            <a>MVP Development</a>
+            <Link to={'.'} className={'hover:underline'}>
+              E-commerce
+            </Link>
+            <Link to={'.'} className={'hover:underline'}>
+              Corporate Website
+            </Link>
+            <Link to={'.'} className={'hover:underline'}>
+              MVP Development
+            </Link>
           </div>
         </div>
-        <div className={'flex flex-col gap-2'}>
+        <div className={'flex flex-col gap-4 sm:text-center'}>
           <h4 className={'text-lg font-semibold italic'}>Legal Information</h4>
           <div className={'flex flex-col gap-2 text-sm font-light'}>
-            <a>Terms of services</a>
-            <a>Privacy Policy</a>
-            <a>Sitemap</a>
+            <Link to={'.'} className={'hover:underline'}>
+              Terms of services
+            </Link>
+            <Link to={'.'} className={'hover:underline'}>
+              Privacy Policy
+            </Link>
           </div>
         </div>
-        <div className={'flex flex-col gap-2 text-right'}>
+        <div className={'flex flex-col gap-4 sm:text-right'}>
           <h4 className={'text-lg font-semibold italic'}>Follow us</h4>
           <div className={'flex flex-col gap-2 text-sm font-light'}>
-            <a>Facebook</a>
-            <a>Twitter</a>
-            <a>Instagram</a>
+            <Link to={'.'} className={'hover:underline'}>
+              Facebook
+            </Link>
+            <Link to={'.'} className={'hover:underline'}>
+              Twitter
+            </Link>
+            <Link to={'.'} className={'hover:underline'}>
+              Linkedin
+            </Link>
           </div>
         </div>
       </div>
       <div
         className={
-          'flex w-3/4 flex-col justify-between gap-8 sm:w-full sm:flex-row md:w-3/4'
+          'flex w-3/4 flex-row justify-between gap-8 sm:w-full md:w-3/4'
         }
       >
-        <h3 className={'w-1/2 text-sm font-extralight italic'}>
+        <h3 className={'w-full text-sm font-extralight italic md:w-1/2'}>
           Made with ❤️&nbsp; by Initial Solutions Team
         </h3>
-        <span className={'w-1/2 text-right text-sm font-extralight italic'}>
+        <span
+          className={
+            'w-full text-sm font-extralight italic sm:text-right md:w-1/2'
+          }
+        >
           2025 © Initial Solutions
         </span>
       </div>
